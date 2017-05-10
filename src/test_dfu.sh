d8712bdd21182c124745d329d76fbf42f6b24ca6
Haoyao Chen <chy@intorobot.com>
2017-05-10 21:06:23 +0800

    add system type detection

--- src/test_dfu.sh
+++ src/test_dfu.sh
@@ -3,6 +3,12 @@
 cd ../
 make
 cd src
-stty -F /dev/cu.usbmodem1411 19200
+sysType=`uname -s`
+if [ $sysType = "Linux" ]; then
+  stty -F /dev/ttyACM0 19200
+elif [ $sysType = "Darwin" ]; then
+  stty -F /dev/cu.usbmodem1411 19200
+fi
+
 sleep 2
 ./dfu-util -d 0x0483:0xdf11 -a 0 -R -s 0x08020000:leave -D default-neutron.dfu
